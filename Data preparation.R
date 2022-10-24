b12e83768029fc267d7ea0078859b2f18d976ca3
ichenhaoyi@gmail.com <email>
2022-10-24 10:03:53 +0200

    loading the data

--- Data preparation.R
+++ Data preparation.R
@@ -1,5 +1,11 @@
 library(readr)
 library(dplyr)
 library(tidyverse)
+library(readx1)
 
-#Load the original csv file
+#Load the original data file
+
+
+
+data0 <-read_excel("data/POCHA_FULL_DATASET_names.xlsx", sheet = 1)
+head(data0)
