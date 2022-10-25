825311935100074173827db9b3ddcb82aa3bdca2
ichenhaoyi@gmail.com <email>
2022-10-25 09:43:43 +0200

    commit

--- Data preparation.R
+++ Data preparation.R
@@ -1,7 +1,7 @@
 library(readr)
 library(dplyr)
 library(tidyverse)
-library(readx1)
+library(readxl)
 
 #Load the original data file
 data_names <-read_excel("data/POCHA_FULL_DATASET_names.xlsx", sheet = 1)
